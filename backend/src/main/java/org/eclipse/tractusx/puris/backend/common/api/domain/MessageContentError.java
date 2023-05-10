7ee5cd46bdc1171550c408fa6a23f478a866af41
Tom Meyer <tom.meyer@isst.fraunhofer.de>
2023-05-10 19:27:37 +0200

    feat: initial model classes for api basis

---
+++ backend/src/main/java/org/eclipse/tractusx/puris/backend/common/api/domain/MessageContentError.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2023 Volkswagen AG
+ * Copyright (c) 2023 Fraunhofer-Gesellschaft zur Foerderung der angewandten Forschung e.V. (represented by Fraunhofer ISST)
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.eclipse.tractusx.puris.backend.common.api.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Implementation of a {@link MessageContent} providing generic error messages.
+ */
+@Entity
+@Table(name = "MessageContentError")
+@DiscriminatorValue("error")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class MessageContentError extends MessageContent {
+
+    /**
+     * identifies a business object of a specific api that refers to the error.
+     */
+    @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
+    @JoinColumn(name = "reference_identification_uuid")
+    private ReferenceIdentification referenceIdentification;
+
+    /**
+     * error code specifying the error.
+     */
+    @NotNull
+    private String error;
+
+    /**
+     * the actual error message providing further detail.
+     */
+    @NotNull
+    private String message;
+}
---
+++ backend/src/main/java/org/eclipse/tractusx/puris/backend/common/api/domain/Response.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2023 Volkswagen AG
+ * Copyright (c) 2023 Fraunhofer-Gesellschaft zur Foerderung der angewandten Forschung e.V. (represented by Fraunhofer ISST)
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.eclipse.tractusx.puris.backend.common.api.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.eclipse.tractusx.puris.backend.common.api.domain.datatype.DT_RequestStateEnum;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * This Response represents the message received via a Response API.
+ * <p>
+ * This Response may not be confused with an HTTP response.
+ * Both, the Response and the Request, are called (api) request.
+ */
+@Entity
+@Table(name = "Response")
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Response {
+
+    /**
+     * This ID prevents the application from collision with external IDs, because the partner
+     * creates the request when performing a Request API call.
+     */
+    @Id
+    @GeneratedValue
+    private UUID internalRequestUuid;
+
+    /**
+     * State of the request.
+     *
+     * @See DT_RequestStateEnum
+     */
+    @NotNull
+    private DT_RequestStateEnum state;
+
+    @OneToMany
+    @JoinColumn(name = "message_content_id")
+    @ToString.Exclude
+    /**
+     * Actual content of the request (or response) message.
+     */
+    private List<MessageContent> messageContents;
+}
