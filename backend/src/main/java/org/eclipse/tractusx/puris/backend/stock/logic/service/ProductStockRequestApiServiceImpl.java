f50013fcd6852e11779b238420dc94787a727998
Tom Meyer <tom.meyer@isst.fraunhofer.de>
2023-05-14 08:30:51 +0200

    feat: product stock preparation for API

---
+++ backend/src/main/java/org/eclipse/tractusx/puris/backend/stock/logic/service/ProductStockRequestApiServiceImpl.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) 2023 Volkswagen AG
+ * Copyright (c) 2023 Fraunhofer-Gesellschaft zur Foerderung der angewandten Forschung e.V.
+ * (represented by Fraunhofer ISST)
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.eclipse.tractusx.puris.backend.stock.logic.service;
+
+import org.eclipse.tractusx.puris.backend.common.api.domain.model.Request;
+import org.eclipse.tractusx.puris.backend.common.api.logic.service.RequestApiService;
+import org.hibernate.cfg.NotYetImplementedException;
+import org.springframework.stereotype.Component;
+
+/**
+ * Service implements the handling of a request for Product Stock
+ * <p>
+ * That means that one need to lookup
+ * {@link org.eclipse.tractusx.puris.backend.stock.domain.model.ProductStock} and return it
+ * according to the API speicfication.
+ */
+@Component
+public class ProductStockRequestApiServiceImpl implements RequestApiService {
+
+    @Override
+    public void handleRequest(Request request) {
+        throw new NotYetImplementedException("Implement Request Flow for " +
+                "ProductStockRequestApiService");
+    }
+}
