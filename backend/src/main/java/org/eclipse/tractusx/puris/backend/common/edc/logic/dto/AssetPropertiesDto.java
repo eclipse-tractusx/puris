dc7c67da3df5315a8cf2f0c5fe5c64b0d5385349
Tom Meyer <tom.meyer@isst.fraunhofer.de>
2023-05-15 07:41:04 +0200

    feat: added CreateAssetDto for Edc package

---
+++ backend/src/main/java/org/eclipse/tractusx/puris/backend/common/edc/logic/dto/AssetPropertiesDto.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) 2023 Volkswagen AG
+ * Copyright (c) 2023 Fraunhofer-Gesellschaft zur Foerderung der angewandten Forschung e.V. (represented by Fraunhofer ISST)
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.eclipse.tractusx.puris.backend.common.edc.logic.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.eclipse.tractusx.puris.backend.common.api.domain.model.datatype.DT_UseCaseEnum;
+import org.eclipse.tractusx.puris.backend.common.edc.logic.dto.datatype.DT_ApiBusinessObjectEnum;
+import org.eclipse.tractusx.puris.backend.common.edc.logic.dto.datatype.DT_ApiMethodEnum;
+import org.eclipse.tractusx.puris.backend.common.edc.logic.dto.datatype.DT_AssetTypeEnum;
+
+/**
+ * Type for asset.property.apibusinessobject of an EDC Asset
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class AssetPropertiesDto {
+
+    /**
+     * id of the asset
+     */
+    @JsonAlias("asset:prop:id")
+    @NotNull
+    private String id;
+
+    @JsonAlias("asset:prop:name")
+    @NotNull
+    private String name;
+
+    @JsonAlias("asset:prop:contenttype")
+    @NotNull
+    private String contentType;
+
+    @JsonAlias("asset:prop:usecase")
+    @NotNull
+    private DT_UseCaseEnum useCase;
+
+    @JsonAlias("asset:prop:type")
+    @NotNull
+    private DT_AssetTypeEnum type;
+
+    @JsonAlias("asset:prop:apibusinessobject")
+    @NotNull
+    private DT_ApiBusinessObjectEnum apiBusinessObject;
+
+    @JsonAlias("asset:prop:apimethod")
+    @NotNull
+    private DT_ApiMethodEnum apiMethod;
+}
---
+++ backend/src/main/java/org/eclipse/tractusx/puris/backend/common/edc/logic/dto/DataAddressPropertiesDto.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2023 Volkswagen AG
+ * Copyright (c) 2023 Fraunhofer-Gesellschaft zur Foerderung der angewandten Forschung e.V. (represented by Fraunhofer ISST)
+ * Copyright (c) 2023 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package org.eclipse.tractusx.puris.backend.common.edc.logic.dto;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.eclipse.tractusx.puris.backend.common.edc.logic.dto.datatype.DT_DataAddressTypeEnum;
+
+/**
+ * Type for asset.dataAddress.properties of an EDC Asset during creation ({@link CreateAssetDto})
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+public class DataAddressPropertiesDto {
+
+    /**
+     * address where to get the asset from
+     */
+    @JsonAlias("baseUrl")
+    @NotNull
+    private String baseUrl;
+
+    /**
+     * Allows to set other Http methods like POST, PUT, DELETE for Http Proxy
+     */
+    @JsonAlias("proxyMethod")
+    @NotNull
+    private boolean proxyMethod;
+
+    /**
+     * Allows to proxy the body through Http Proxy for e.g. an API call.
+     */
+    @JsonAlias("proxyBody")
+    @NotNull
+    private boolean proxyBody;
+
+    /**
+     * Defines the type of data asset e.g. HttpData
+     */
+    @JsonAlias("type")
+    @NotNull
+    private DT_DataAddressTypeEnum type;
+}
