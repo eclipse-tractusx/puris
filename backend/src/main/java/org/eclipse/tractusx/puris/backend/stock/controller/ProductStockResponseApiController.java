9f5c50b1ae719eb425d6fdcd269eb4b4f3f7ea49
Ernst-Christoph Schrewe <ernst-christoph.schrewe@isst.fraunhofer.de>
2023-07-25 09:00:10 +0200

    fix: productstockresponseapicontroller now returns status codes as defined also removed inheritance from responseapicontroller for the same reason as requestapicontroller was removed

--- backend/src/main/java/org/eclipse/tractusx/puris/backend/stock/controller/ProductStockResponseApiController.java
+++ backend/src/main/java/org/eclipse/tractusx/puris/backend/stock/controller/ProductStockResponseApiController.java
@@ -21,15 +21,24 @@
  */
 package org.eclipse.tractusx.puris.backend.stock.controller;
 
-import org.eclipse.tractusx.puris.backend.common.api.controller.ResponseApiController;
+import java.util.UUID;
+
+import org.eclipse.tractusx.puris.backend.common.api.domain.model.Request;
+import org.eclipse.tractusx.puris.backend.common.api.domain.model.datatype.DT_RequestStateEnum;
+import org.eclipse.tractusx.puris.backend.common.api.logic.dto.SuccessfullRequestDto;
+import org.eclipse.tractusx.puris.backend.common.api.logic.service.RequestService;
 import org.eclipse.tractusx.puris.backend.common.api.logic.service.ResponseApiService;
 import org.eclipse.tractusx.puris.backend.stock.logic.dto.ProductStockResponseDto;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import lombok.extern.slf4j.Slf4j;
@@ -37,26 +46,51 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @RequestMapping("product-stock")
 @Slf4j
-public class ProductStockResponseApiController extends ResponseApiController {
+public class ProductStockResponseApiController {
+
+    @Autowired
+    RequestService requestService;
+
+    @Autowired
+    ModelMapper modelMapper;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    private ResponseApiService responseApiService;
 
     public ProductStockResponseApiController(ResponseApiService responseApiService) {
-        super(responseApiService);
+        this.responseApiService = responseApiService;
     }
 
 
     @PostMapping("response")
-    public ResponseEntity postResponse(@RequestBody ProductStockResponseDto productStockResponseDto) {
-        log.info("product-stock/response called");
-        
-        ObjectMapper objectMapper = new ObjectMapper();
+    public ResponseEntity<Object> postResponse(@RequestBody JsonNode requestBody) {
+        log.info("product-stock/response called: \n" + requestBody.toPrettyString());
+        ProductStockResponseDto productStockResponseDto = null;
         try {
-            var jsonString = objectMapper.writeValueAsString(productStockResponseDto);
-            jsonString = objectMapper.readTree(jsonString).toPrettyString();
-            log.info("\n" + jsonString);
-        } catch (Exception e){
-
+            productStockResponseDto = objectMapper.treeToValue(requestBody, ProductStockResponseDto.class);
+        } catch (Exception e) {
+            log.error("Failed to deserialize body of incoming message", e);
+            return ResponseEntity.status(HttpStatusCode.valueOf(422)).build();
         }
-        return super.postResponse(productStockResponseDto);
+        
+        UUID requestId = productStockResponseDto.getHeader().getRequestId();
+
+        Request requestFound =
+                requestService.findRequestByHeaderUuid(requestId);
+        if (requestFound == null) {
+            log.error("Request id " +requestId +  " not found");
+            return ResponseEntity.status(422).build();
+        } else {
+            log.info("Got response for request Id " + requestId);
+        }
+
+        requestFound = requestService.updateState(requestFound, DT_RequestStateEnum.COMPLETED);
+        responseApiService.consumeResponse(productStockResponseDto);
+
+        // if the request has been correctly taken over, return 202
+        return ResponseEntity.status(HttpStatusCode.valueOf(202)).body(new SuccessfullRequestDto(requestId));
     }
 
 }
