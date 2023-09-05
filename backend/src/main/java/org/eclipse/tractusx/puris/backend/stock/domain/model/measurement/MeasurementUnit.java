70cb48015e8ab4ad2efbb9436427d56af4d39a46
Ernst-Christoph Schrewe <ernst-christoph.schrewe@isst.fraunhofer.de>
2023-09-05 14:12:41 +0200

    feat: added enum measurementunit

---
+++ backend/src/main/java/org/eclipse/tractusx/puris/backend/stock/domain/model/measurement/MeasurementUnit.java
@@ -0,0 +1,34 @@
+package org.eclipse.tractusx.puris.backend.stock.domain.model.measurement;
+
+public enum MeasurementUnit {
+
+    gram,
+    kilogram,
+    tonneMetricTon,
+    tonUsOrShortTonUkorus,
+    ounceAvoirdupois,
+    pound,
+    centimetre,
+    metre,
+    kilometre,
+    inch,
+    foot,
+    yard,
+    squareCentimetre,
+    squareMetre,
+    squareInch,
+    squareFoot,
+    squareYard,
+    cubicCentimetre,
+    cubicMetre,
+    cubicInch,
+    millilitre,
+    litre,
+    hectolitre,
+    piece,
+    set,
+    pair,
+    page,
+    kilowattHour
+
+}
