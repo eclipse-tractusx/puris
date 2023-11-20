83d99a5a61fce28de4118cde46a582b1c2fd31ea
--show-origin <tom.meyer@isst.fraunhofer.de>
2023-11-20 09:43:48 -0800

    fix: removed context from mockMvc paths

--- backend/src/test/java/org/eclipse/tractusx/puris/backend/common/security/ApiKeyTest.java
+++ backend/src/test/java/org/eclipse/tractusx/puris/backend/common/security/ApiKeyTest.java
@@ -1,12 +1,9 @@
 package org.eclipse.tractusx.puris.backend.common.security;
 
-import org.eclipse.tractusx.puris.backend.AssetCreatorCommandLineRunner;
-import org.eclipse.tractusx.puris.backend.DataInjectionCommandLineRunner;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -21,29 +18,22 @@ public class ApiKeyTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
-    private AssetCreatorCommandLineRunner assetCreatorCommandLineRunner;
-
-    @MockBean
-    private DataInjectionCommandLineRunner dataInjectionCommandLineRunner;
-
     @Test
     void stockViewShouldReturn403WithoutAuth() throws Exception {
         this.mockMvc.perform(
-            get("/catena/stockView/materials"))
+            get("/stockView/materials"))
                 .andDo(print())
             .andExpect(status().is(403));
     }
 
     @Test
-    @WithMockApiKey
+//    @WithMockApiKey // not yet working
     void stockViewShouldReturn200WithAuth() throws Exception {
         this.mockMvc.perform(
-                get("/catena/stockView/materials")
-                    .header("X-API-KEY", "test"))
-            //.andDo(print())
+                get("/stockView/materials")
+                    .header("X-API-KEY", "test")
+            )
             .andExpect(status().is(200));
-        // WTF??
     }
 
 }
