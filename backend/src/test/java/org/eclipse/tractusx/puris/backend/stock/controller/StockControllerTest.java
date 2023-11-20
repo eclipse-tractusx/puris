220763839e26428fb1a150870036d85f7dd3cde6
--show-origin <tom.meyer@isst.fraunhofer.de>
2023-11-20 10:15:07 -0800

    fix: got security included material service working

--- backend/src/test/java/org/eclipse/tractusx/puris/backend/stock/controller/StockControllerTest.java
+++ backend/src/test/java/org/eclipse/tractusx/puris/backend/stock/controller/StockControllerTest.java
@@ -1,9 +1,14 @@
 package org.eclipse.tractusx.puris.backend.stock.controller;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.eclipse.tractusx.puris.backend.common.security.SecurityConfig;
+import org.eclipse.tractusx.puris.backend.common.security.logic.ApiKeyAuthenticationProvider;
 import org.eclipse.tractusx.puris.backend.masterdata.domain.model.Material;
 import org.eclipse.tractusx.puris.backend.masterdata.logic.service.MaterialPartnerRelationService;
 import org.eclipse.tractusx.puris.backend.masterdata.logic.service.MaterialService;
 import org.eclipse.tractusx.puris.backend.masterdata.logic.service.PartnerService;
+import org.eclipse.tractusx.puris.backend.stock.logic.dto.FrontendMaterialDto;
 import org.eclipse.tractusx.puris.backend.stock.logic.service.MaterialStockService;
 import org.eclipse.tractusx.puris.backend.stock.logic.service.PartnerProductStockService;
 import org.eclipse.tractusx.puris.backend.stock.logic.service.ProductStockRequestApiService;
@@ -13,26 +18,23 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @WebMvcTest(StockViewController.class)
+@Import({ SecurityConfig.class, ApiKeyAuthenticationProvider.class })
 class StockControllerTest {
-
-//    @LocalServerPort
-//    private int port;
-
-//    @Autowired
-//    private TestRestTemplate restTemplate;
 
     @Autowired
     private MockMvc mockMvc;
@@ -61,17 +63,6 @@ class StockControllerTest {
     @MockBean
     private ModelMapper modelMapper;
 
-    //@InjectMocks
-    //@MockBean
-    //StockController underTest;
-
-//    @BeforeEach
-//    void setUp() {
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//    }
 
     @Test
     void getMaterials() throws Exception{
@@ -96,27 +87,37 @@ class StockControllerTest {
         // 401 returned due to https://stackoverflow.com/questions/39554285/spring-test-returning-401-for-unsecured-urls
         // problem: we need security
         this.mockMvc.perform(
-                get("/catena/stockView/materials")
-            ).andExpect(status().isOk())
-            .andExpect(content().contentType(MediaType.APPLICATION_JSON));
+                get("/stockView/materials")
+                    .header("X-API-KEY", "test")
+            )
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+            .andDo(result -> {
+                String jsonResponse = result.getResponse().getContentAsString();
+                ObjectMapper objectMapper = new ObjectMapper();
 
-        // then
-//        assertAll(
-//            () -> assertNotNull(returnedMaterials),
-//            () -> assertEquals(2, returnedMaterials.size())
-//        );
-//
-//        FrontendMaterialDto returnedMaterial = returnedMaterials.stream().filter(
-//            frontendMaterialDto -> frontendMaterialDto.getOwnMaterialNumber().equals("MNR-4711")
-//        ).findFirst().get();
-//        assertAll(
-//            () -> assertEquals("MNR-4711", returnedMaterial.getOwnMaterialNumber()),
-//            () -> assertEquals("Test Material 1", returnedMaterial.getDescription())
-//        );
+                List<FrontendMaterialDto> returnedMaterials = objectMapper.readValue(jsonResponse, new TypeReference<>() {
+                });
 
+                assertAll(
+                    () -> assertNotNull(returnedMaterials),
+                    () -> assertEquals(2, returnedMaterials.size())
+                );
 
-        //this.mockMvc.perform(get("/materials")).andDo(print()).andExpect(status().isOk())
-        //    .andExpect(content().contentType().);
+                assertAll(
+                    () -> assertNotNull(returnedMaterials),
+                    () -> assertEquals(2, returnedMaterials.size())
+                );
+
+                FrontendMaterialDto returnedMaterial = returnedMaterials.stream().filter(
+                    frontendMaterialDto -> frontendMaterialDto.getOwnMaterialNumber().equals("MNR-4711")
+                ).findFirst().get();
+                assertAll(
+                    () -> assertEquals("MNR-4711", returnedMaterial.getOwnMaterialNumber()),
+                    () -> assertEquals("Test Material 1", returnedMaterial.getDescription())
+                );
+            });
     }
 //
 //    @Test
