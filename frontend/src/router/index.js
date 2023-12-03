4191985c3d2736e71eff636675183f8bc716f2a9
--show-origin <tom.meyer@isst.fraunhofer.de>
2023-12-03 01:58:14 -0800

    feat: added keycloak idp

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -19,66 +19,122 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { createRouter, createWebHistory } from "vue-router";
+import {createRouter, createWebHistory} from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import AuthenticationService from "@/services/AuthenticationService";
+
+export const ALL_ROUTES = [
+    {
+        path: "/",
+        name: "Dashboard",
+        component: HomeView,
+        meta: {
+            requiredRoles: ["PURIS_USER"],
+        },
+    },
+    {
+        path: "/createOrder",
+        name: "Create Order",
+        // route level code-splitting
+        // this generates a separate chunk (About.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import("../views/CreateOrderView.vue"),
+        meta: {
+            requiredRoles: ["PURIS_ADMIN"],
+        },
+    },
+    {
+        path: "/manageOrders",
+        name: "Manage Orders",
+        component: () => import("../views/ManageOrderView.vue"),
+        meta: {
+            requiredRoles: ["PURIS_ADMIN"],
+        },
+    },
+    {
+        path: "/catalog",
+        name: "Catalog",
+        component: () => import("../views/CatalogView.vue"),
+        meta: {
+            requiredRoles: ["PURIS_ADMIN"],
+        },
+    },
+    {
+        path: "/negotiations",
+        name: "Negotiations",
+        component: () => import("../views/NegotiationView.vue"),
+        meta: {
+            requiredRoles: ["PURIS_ADMIN"],
+        },
+    },
+    {
+        path: "/transfers",
+        name: "Transfers",
+        component: () => import("../views/TransferView.vue"),
+        meta: {
+            requiredRoles: ["PURIS_ADMIN"],
+        },
+    },
+    {
+        path: "/responses",
+        name: "Responses",
+        component: () => import("../views/OrderResponseView.vue"),
+        meta: {
+            requiredRoles: ["PURIS_ADMIN"],
+        },
+    },
+    {
+        path: "/connectors",
+        name: "Connectors",
+        component: () => import("../views/ConnectorView.vue"),
+        meta: {
+            requiredRoles: ["PURIS_ADMIN"],
+        },
+    },
+    {
+        path: "/stocks",
+        name: "Stocks",
+        component: () => import("../views/StockView.vue"),
+        meta: {
+            requiredRoles: ["PURIS_USER"],
+        },
+    },
+    {
+        path: "/supplierDashboard",
+        name: "Supplier Dashboard",
+        component: () => import("../views/SupplierDashboard.vue"),
+        meta: {
+            requiredRoles: ["PURIS_USER"],
+        },
+    },
+    {
+        path: "/unauthorized",
+        name: "Unauthorized",
+        component: () => import("../views/Unauthorized.vue"),
+    },
+];
+
+const requireRole = (to, from, next) => {
+    const requiredRoles = to.meta.requiredRoles;
+
+    if (!requiredRoles) {
+        next();
+    }
+
+    if (AuthenticationService.userHasRole(requiredRoles)) {
+        next();
+    } else {
+        next("/unauthorized");
+    }
+};
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: HomeView,
-    },
-    {
-      path: "/createOrder",
-      name: "createOrder",
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import("../views/CreateOrderView.vue"),
-    },
-    {
-      path: "/manageOrders",
-      name: "manageOrders",
-      component: () => import("../views/ManageOrderView.vue"),
-    },
-    {
-      path: "/catalog",
-      name: "catalog",
-      component: () => import("../views/CatalogView.vue"),
-    },
-    {
-      path: "/negotiations",
-      name: "negotitations",
-      component: () => import("../views/NegotiationView.vue"),
-    },
-    {
-      path: "/transfers",
-      name: "transfers",
-      component: () => import("../views/TransferView.vue"),
-    },
-    {
-      path: "/responses",
-      name: "responses",
-      component: () => import("../views/OrderResponseView.vue"),
-    },
-    {
-      path: "/connectors",
-      name: "connectors",
-      component: () => import("../views/ConnectorView.vue"),
-    },
-    {
-      path: "/stocks",
-      name: "stocks",
-      component: () => import("../views/StockView.vue")
-    },
-    {
-      path: "/supplierDashboard",
-      name: "supplierDashboard",
-      component: () => import("../views/SupplierDashboard.vue"),
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: ALL_ROUTES,
+});
+
+router.beforeEach((to, from, next) => {
+    requireRole(to, from, next);
 });
 
 export default router;
