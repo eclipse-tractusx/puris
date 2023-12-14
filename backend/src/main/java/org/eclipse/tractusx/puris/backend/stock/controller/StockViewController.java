8eef69e0b033313d75dabf020cba01391e0b124d
Ernst-Christoph Schrewe <ernst-christoph.schrewe@isst.fraunhofer.de>
2023-12-14 11:00:35 +0100

    fix: removed quickfix

--- backend/src/main/java/org/eclipse/tractusx/puris/backend/stock/controller/StockViewController.java
+++ backend/src/main/java/org/eclipse/tractusx/puris/backend/stock/controller/StockViewController.java
@@ -313,9 +313,6 @@ public class StockViewController {
         if(!materialPattern.matcher(ownMaterialNumber).matches()) {
             return new ResponseEntity<>(HttpStatusCode.valueOf(400));
         }
-        ownMaterialNumber = ownMaterialNumber.replace("\n","");
-        ownMaterialNumber = ownMaterialNumber.replace("\r", "");
-        ownMaterialNumber = ownMaterialNumber.replace("|","");
         Material materialEntity = materialService.findByOwnMaterialNumber(ownMaterialNumber);
         log.info("Found material: " + (materialEntity != null) + " " + ownMaterialNumber);
         List<Partner> allSupplierPartnerEntities = mprService.findAllSuppliersForOwnMaterialNumber(ownMaterialNumber);
