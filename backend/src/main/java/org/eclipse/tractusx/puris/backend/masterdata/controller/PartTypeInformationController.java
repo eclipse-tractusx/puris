4d6325a2fa76fdeee14ac9cf6ae0189b9473750b
Ernst-Christoph Schrewe <ernst-christoph.schrewe@isst.fraunhofer.de>
2024-03-15 09:21:54 +0100

    chore: added license header and swagger to parttype endpoint

--- backend/src/main/java/org/eclipse/tractusx/puris/backend/masterdata/controller/PartTypeInformationController.java
+++ backend/src/main/java/org/eclipse/tractusx/puris/backend/masterdata/controller/PartTypeInformationController.java
@@ -1,5 +1,28 @@
+/*
+ * Copyright (c) 2024 Volkswagen AG
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License, Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 package org.eclipse.tractusx.puris.backend.masterdata.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.tractusx.puris.backend.common.util.PatternStore;
 import org.eclipse.tractusx.puris.backend.masterdata.domain.model.Material;
@@ -33,6 +56,15 @@ public class PartTypeInformationController {
     @Autowired
     private PartTypeInformationSammMapper sammMapper;
 
+    @Operation(description = "Ednpoint that delivers PartTypeInformation of own products to customer partners. " +
+        "'bpnl' must be set to the bpnl of the requesting party. 'materialnumber' must be set to the ownMaterialNumber of " +
+        "the party, that receives the request")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Ok"),
+        @ApiResponse(responseCode = "400", description = "Invalid request parameters. "),
+        @ApiResponse(responseCode = "401", description = "Access forbidden. "),
+        @ApiResponse(responseCode = "404", description = "Product not found for given parameters. ")
+    })
     @GetMapping("/{bpnl}/{materialnumber}")
     public ResponseEntity<?> getMapping(@PathVariable String bpnl, @PathVariable String materialnumber) {
         if (!bpnlPattern.matcher(bpnl).matches() || !materialNumberPattern.matcher(materialnumber).matches()) {
