f85dcff474f79be8479b2814e1ac2c5528058d98
Ernst-Christoph Schrewe <ernst-christoph.schrewe@isst.fraunhofer.de>
2024-04-10 11:46:40 +0200

    chore: initial commit

--- backend/src/main/java/org/eclipse/tractusx/puris/backend/stock/controller/ItemStockRequestApiController.java
+++ backend/src/main/java/org/eclipse/tractusx/puris/backend/stock/controller/ItemStockRequestApiController.java
@@ -23,23 +23,15 @@ package org.eclipse.tractusx.puris.backend.stock.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import jakarta.validation.Validator;
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.tractusx.puris.backend.stock.domain.model.datatype.DT_RequestStateEnum;
 import org.eclipse.tractusx.puris.backend.common.util.PatternStore;
-import org.eclipse.tractusx.puris.backend.stock.domain.model.ItemStockRequestMessage;
-import org.eclipse.tractusx.puris.backend.stock.logic.dto.ItemStockRequestMessageDto;
-import org.eclipse.tractusx.puris.backend.stock.logic.dto.ItemStockStatusRequestMessageDto;
 import org.eclipse.tractusx.puris.backend.stock.logic.dto.itemstocksamm.DirectionCharacteristic;
 import org.eclipse.tractusx.puris.backend.stock.logic.dto.itemstocksamm.ItemStockSamm;
 import org.eclipse.tractusx.puris.backend.stock.logic.service.ItemStockRequestApiService;
-import org.eclipse.tractusx.puris.backend.stock.logic.service.ItemStockRequestMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
-import java.util.concurrent.ExecutorService;
 import java.util.regex.Pattern;
 
 @RestController
@@ -53,12 +45,7 @@ public class ItemStockRequestApiController {
 
     @Autowired
     private ItemStockRequestApiService itemStockRequestApiService;
-    @Autowired
-    private ItemStockRequestMessageService itemStockRequestMessageService;
-    @Autowired
-    private ExecutorService executorService;
-    @Autowired
-    private Validator validator;
+
     private final Pattern bpnlPattern = PatternStore.BPNL_PATTERN;
 
     private final Pattern urnPattern = PatternStore.URN_OR_UUID_PATTERN;
@@ -95,66 +82,4 @@ public class ItemStockRequestApiController {
         }
         return ResponseEntity.ok(samm);
     }
-
-
-    @Operation(summary = "This endpoint receives the item stock request messages. ")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "202", description = "The request was accepted"),
-        @ApiResponse(responseCode = "400", description = "Bad request"),
-        @ApiResponse(responseCode = "422", description = "A request with same Id already exists")
-    })
-    @PostMapping("request")
-    public ResponseEntity<RequestReactionMessageDto> postMapping(@RequestBody ItemStockRequestMessageDto requestMessageDto) {
-        if (!validator.validate(requestMessageDto).isEmpty()) {
-            log.warn("Rejected invalid message body");
-            return ResponseEntity.status(400).body(new RequestReactionMessageDto(requestMessageDto.getHeader().getMessageId()));
-        }
-        log.info("Got Request\n" + requestMessageDto);
-        ItemStockRequestMessage requestMessage = ItemStockRequestMessageDto.convertToEntity(requestMessageDto);
-        var createdRequestMessage = itemStockRequestMessageService.create(requestMessage);
-        log.info("Created RequestMessageEntity:\n" + requestMessage);
-        if (createdRequestMessage == null) {
-            // Validation failed or messageId was used before in combination with these partner bpnl's
-            log.warn("Received invalid request\n" + requestMessageDto);
-            return ResponseEntity.status(422).body(new RequestReactionMessageDto(requestMessageDto.getHeader().getMessageId()));
-        }
-        switch (requestMessageDto.getContent().getDirection()) {
-            case OUTBOUND ->
-                executorService.submit(() -> itemStockRequestApiService.handleRequestFromCustomer(requestMessageDto, createdRequestMessage));
-            case INBOUND ->
-                executorService.submit(() -> itemStockRequestApiService.handleRequestFromSupplier(requestMessageDto, createdRequestMessage));
-            default -> {
-                log.warn("Missing direction in request \n" + requestMessageDto);
-            }
-        }
-        return ResponseEntity.status(202).body(new RequestReactionMessageDto(requestMessageDto.getHeader().getMessageId()));
-    }
-
-    @Operation(summary = "This endpoint receives the item stock status request messages. ")
-    @ApiResponses(value = {
-        @ApiResponse(responseCode = "200", description = "Status request was successful. "),
-        @ApiResponse(responseCode = "400", description = "Bad Request"),
-        @ApiResponse(responseCode = "422", description = "Unknown Message Id requested")
-    })
-    @PostMapping("status")
-    public ResponseEntity<StatusReactionMessageDto> getStatus(@RequestBody ItemStockStatusRequestMessageDto statusRequest) {
-        if (!validator.validate(statusRequest).isEmpty()) {
-            // Bad Request
-            log.warn("Rejected invalid message body");
-            return ResponseEntity.status(400).build();
-        }
-        var relatedMessage = itemStockRequestMessageService.find(new ItemStockRequestMessage.Key(statusRequest.getHeader().getMessageId(),
-            statusRequest.getHeader().getSenderBpn(), statusRequest.getHeader().getReceiverBpn()));
-        if (relatedMessage == null) {
-            return ResponseEntity.status(422).build();
-        }
-        return ResponseEntity.status(200).body(new StatusReactionMessageDto(statusRequest.getHeader().getRelatedMessageId(), relatedMessage.getState()));
-    }
-
-    private record RequestReactionMessageDto(UUID messageId) {
-    }
-
-    private record StatusReactionMessageDto(UUID messageId, DT_RequestStateEnum requestState) {
-    }
-
 }
--- backend/src/main/java/org/eclipse/tractusx/puris/backend/stock/logic/service/ItemStockRequestApiService.java
+++ backend/src/main/java/org/eclipse/tractusx/puris/backend/stock/logic/service/ItemStockRequestApiService.java
@@ -22,26 +22,17 @@ package org.eclipse.tractusx.puris.backend.stock.logic.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
-import org.eclipse.tractusx.puris.backend.common.edc.logic.dto.datatype.DT_ApiMethodEnum;
 import org.eclipse.tractusx.puris.backend.common.edc.logic.service.EdcAdapterService;
-import org.eclipse.tractusx.puris.backend.common.util.VariablesService;
 import org.eclipse.tractusx.puris.backend.masterdata.domain.model.Material;
 import org.eclipse.tractusx.puris.backend.masterdata.domain.model.Partner;
 import org.eclipse.tractusx.puris.backend.masterdata.logic.service.MaterialPartnerRelationService;
 import org.eclipse.tractusx.puris.backend.masterdata.logic.service.MaterialService;
 import org.eclipse.tractusx.puris.backend.masterdata.logic.service.PartnerService;
-import org.eclipse.tractusx.puris.backend.stock.domain.model.ItemStockRequestMessage;
-import org.eclipse.tractusx.puris.backend.stock.domain.model.datatype.DT_RequestStateEnum;
 import org.eclipse.tractusx.puris.backend.stock.logic.adapter.ItemStockSammMapper;
-import org.eclipse.tractusx.puris.backend.stock.logic.dto.ItemStockRequestMessageDto;
-import org.eclipse.tractusx.puris.backend.stock.logic.dto.ItemStockResponseDto;
 import org.eclipse.tractusx.puris.backend.stock.logic.dto.itemstocksamm.DirectionCharacteristic;
 import org.eclipse.tractusx.puris.backend.stock.logic.dto.itemstocksamm.ItemStockSamm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Date;
-import java.util.UUID;
 
 @Service
 @Slf4j
@@ -64,15 +55,11 @@ public class ItemStockRequestApiService {
     @Autowired
     private ReportedMaterialItemStockService reportedMaterialItemStockService;
     @Autowired
-    private VariablesService variablesService;
-    @Autowired
     private EdcAdapterService edcAdapterService;
     @Autowired
     private ItemStockSammMapper sammMapper;
     @Autowired
     private ObjectMapper objectMapper;
-    @Autowired
-    private ItemStockRequestMessageService itemStockRequestMessageService;
 
     public ItemStockSamm handleItemStockSubmodelRequest(String bpnl, String materialNumber, DirectionCharacteristic direction) {
         Partner partner = partnerService.findByBpnl(bpnl);
@@ -170,265 +157,6 @@ public class ItemStockRequestApiService {
             log.info("Updated ReportedProductItemStocks for " + material.getOwnMaterialNumber() + " and partner " + partner.getBpnl());
         } catch (Exception e) {
             log.error("Error in ReportedProductItemStockRequest for " + material.getOwnMaterialNumber() + " and partner " + partner.getBpnl(), e);
-        }
-    }
-
-
-    /**
-     * This method receives a request message from a customer and
-     * assembles and sends a response from your local ProductItemStocks
-     * to this customer.
-     *
-     * @param requestMessageDto the RequestMessage
-     */
-    public void handleRequestFromCustomer(ItemStockRequestMessageDto requestMessageDto, ItemStockRequestMessage requestMessage) {
-        var requestMessageHeader = requestMessageDto.getHeader();
-        Partner customerPartner = partnerService.findByBpnl(requestMessageHeader.getSenderBpn());
-        if (customerPartner == null || requestMessageDto.getContent().getDirection() != DirectionCharacteristic.OUTBOUND) {
-            requestMessage.setState(DT_RequestStateEnum.Error);
-            itemStockRequestMessageService.update(requestMessage);
-            if (requestMessageDto.getContent().getDirection() != DirectionCharacteristic.OUTBOUND) {
-                log.error("Wrong direction in request from customer \n" + requestMessageDto);
-            }
-            if (customerPartner == null) {
-                log.error("Unknown Partner in request \n" + requestMessageDto);
-            }
-            return;
-        }
-        requestMessage.setState(DT_RequestStateEnum.Working);
-        itemStockRequestMessageService.update(requestMessage);
-        ItemStockResponseDto responseDto = new ItemStockResponseDto();
-        responseDto.getHeader().setRelatedMessageId(requestMessageHeader.getMessageId());
-
-        for (var productRequest : requestMessageDto.getContent().getItemStock()) {
-            Material product = null;
-            if (productRequest.getMaterialGlobalAssetId() != null) {
-                product = materialService.findByMaterialNumberCx(productRequest.getMaterialGlobalAssetId());
-            }
-            if (product == null && productRequest.getMaterialNumberCustomer() != null) {
-                var foundProducts = mprService.findAllByPartnerMaterialNumber(productRequest.getMaterialNumberCustomer());
-                foundProducts = foundProducts.stream()
-                    .filter(Material::isProductFlag)
-                    .filter(m -> mprService.partnerOrdersProduct(m, customerPartner))
-                    .toList();
-                if (!foundProducts.isEmpty()) {
-                    product = foundProducts.get(0);
-                }
-                if (foundProducts.size() > 1) {
-                    log.warn("Ambiguous material definition in request \n" + productRequest);
-                    log.warn("Arbitrarily choosing " + product.getOwnMaterialNumber());
-                }
-            }
-            if (product == null && productRequest.getMaterialNumberSupplier() != null) {
-                product = materialService.findByOwnMaterialNumber(productRequest.getMaterialNumberCustomer());
-            }
-            if (product == null) {
-                log.error("Could not identify material in " + productRequest);
-                continue;
-            }
-            var productItemStocks = productItemStockService.findByPartnerAndMaterial(customerPartner, product)
-                .stream().filter(itemStock -> itemStock.getQuantity() > 0).toList();
-            responseDto.getContent().getItemStock().add(sammMapper.productItemStocksToItemStockSamm(productItemStocks));
-        }
-        sendResponse(customerPartner, responseDto, requestMessage);
-
-    }
-
-
-    /**
-     * This method receives a request message from a supplier and
-     * assembles and sends a response from your local MaterialItemStocks
-     * to this customer.
-     *
-     * @param requestMessageDto the RequestMessage
-     */
-    public void handleRequestFromSupplier(ItemStockRequestMessageDto requestMessageDto, ItemStockRequestMessage requestMessage) {
-        var requestMessageHeader = requestMessageDto.getHeader();
-        Partner supplierPartner = partnerService.findByBpnl(requestMessageHeader.getSenderBpn());
-        if (supplierPartner == null || requestMessageDto.getContent().getDirection() != DirectionCharacteristic.INBOUND) {
-            requestMessage.setState(DT_RequestStateEnum.Error);
-            itemStockRequestMessageService.update(requestMessage);
-            if (requestMessageDto.getContent().getDirection() != DirectionCharacteristic.INBOUND) {
-                log.error("Wrong direction in request from customer \n" + requestMessageDto);
-            }
-            if (supplierPartner == null) {
-                log.error("Unknown Partner in request \n" + requestMessageDto);
-            }
-            return;
-        }
-
-        requestMessage.setState(DT_RequestStateEnum.Working);
-        itemStockRequestMessageService.update(requestMessage);
-        ItemStockResponseDto responseDto = new ItemStockResponseDto();
-        responseDto.getHeader().setRelatedMessageId(requestMessageHeader.getMessageId());
-        for (var materialRequest : requestMessageDto.getContent().getItemStock()) {
-            Material material = null;
-            if (materialRequest.getMaterialGlobalAssetId() != null) {
-                material = materialService.findByMaterialNumberCx(materialRequest.getMaterialGlobalAssetId());
-            }
-            if (material == null && materialRequest.getMaterialNumberCustomer() != null) {
-                material = materialService.findByOwnMaterialNumber(materialRequest.getMaterialNumberCustomer());
-            }
-            if (material == null && materialRequest.getMaterialNumberSupplier() != null) {
-                var foundMaterials = mprService.findAllByPartnerMaterialNumber(materialRequest.getMaterialNumberSupplier());
-                foundMaterials = foundMaterials.stream()
-                    .filter(Material::isMaterialFlag)
-                    .filter(m -> mprService.partnerSuppliesMaterial(m, supplierPartner))
-                    .toList();
-                if (!foundMaterials.isEmpty()) {
-                    material = foundMaterials.get(0);
-                }
-                if (foundMaterials.size() > 1) {
-                    log.warn("Ambiguous material definition in request \n" + materialRequest);
-                    log.warn("Arbitrarily choosing " + material.getOwnMaterialNumber());
-                }
-            }
-            if (material == null) {
-                log.error("Could not identify material in " + materialRequest);
-                continue;
-            }
-            var materialItemStocks = materialItemStockService.findByPartnerAndMaterial(supplierPartner, material)
-                .stream().filter(itemStock -> itemStock.getQuantity() > 0).toList();
-            responseDto.getContent().getItemStock().add(sammMapper.materialItemStocksToItemStockSamm(materialItemStocks));
-        }
-        sendResponse(supplierPartner, responseDto, requestMessage);
-
-    }
-
-    /**
-     * Convenience method to bundle the common parts in the workflow of
-     * the handleRequestFromCustomer and handleRequestFromSupplier methods.
-     * The responseDto object's content is expected to be filled in advance.
-     *
-     * @param partner     the given Partner
-     * @param responseDto the responseDto
-     */
-    private void sendResponse(Partner partner, ItemStockResponseDto responseDto, ItemStockRequestMessage requestMessage) {
-        var data = edcAdapterService.getContractForItemStockApi(partner, DT_ApiMethodEnum.RESPONSE);
-        if (data == null) {
-            requestMessage.setState(DT_RequestStateEnum.Error);
-            itemStockRequestMessageService.update(requestMessage);
-            log.error("Failed to contract response api from " + partner.getEdcUrl());
-            return;
-        }
-        String authKey = data[0];
-        String authCode = data[1];
-        String endpoint = data[2];
-        var responseDtoHeader = responseDto.getHeader();
-        responseDtoHeader.setMessageId(UUID.randomUUID());
-        responseDtoHeader.setReceiverBpn(partner.getBpnl());
-        responseDtoHeader.setSenderBpn(variablesService.getOwnBpnl());
-        responseDtoHeader.setVersion(ItemStockRequestMessage.VERSION);
-        responseDtoHeader.setContext(ItemStockRequestMessage.CONTEXT);
-        responseDtoHeader.setSentDateTime(new Date());
-        try {
-            String requestBody = objectMapper.writeValueAsString(responseDto);
-            var httpResponse = edcAdapterService.postProxyPullRequest(
-                endpoint, authKey, authCode, requestBody);
-            httpResponse.body().close();
-            log.info("Sent response \n" + responseDto);
-            requestMessage.setState(DT_RequestStateEnum.Completed);
-        } catch (Exception e) {
-            requestMessage.setState(DT_RequestStateEnum.Error);
-            log.error("Failed to send response \n" + responseDto, e);
-        } finally {
-            log.info("Updating state");
-            itemStockRequestMessageService.update(requestMessage);
-        }
-    }
-
-
-    /**
-     * This method creates and sends a request to the given supplier-partner
-     * in order to receive his current ItemStocks for the given materials.
-     *
-     * @param supplierPartner the supplier Partner
-     * @param materials       the materials
-     */
-    public void doRequestForMaterialItemStocks(Partner supplierPartner, Material... materials) {
-        ItemStockRequestMessage itemStockRequestMessage = getItemStockRequestMessage(supplierPartner);
-        itemStockRequestMessage.setDirection(DirectionCharacteristic.OUTBOUND);
-        for (var material : materials) {
-            ItemStockRequestMessage.Request request = new ItemStockRequestMessage.Request();
-            request.setMaterialGlobalAssetId(material.getMaterialNumberCx());
-            request.setMaterialNumberCustomer(material.getOwnMaterialNumber());
-            request.setMaterialNumberSupplier(mprService.find(material, supplierPartner).getPartnerMaterialNumber());
-            itemStockRequestMessage.getItemStock().add(request);
-        }
-        sendRequest(supplierPartner, itemStockRequestMessage);
-    }
-
-    /**
-     * This method creates and sends a request to the given customer-partner
-     * in order to receive his current ItemStocks for the given materials.
-     *
-     * @param customerPartner the supplier Partner
-     * @param materials       the materials
-     */
-    public void doRequestForProductItemStocks(Partner customerPartner, Material... materials) {
-        ItemStockRequestMessage itemStockRequestMessage = getItemStockRequestMessage(customerPartner);
-        itemStockRequestMessage.setDirection(DirectionCharacteristic.INBOUND);
-        for (var material : materials) {
-            ItemStockRequestMessage.Request request = new ItemStockRequestMessage.Request();
-            request.setMaterialGlobalAssetId(material.getMaterialNumberCx());
-            request.setMaterialNumberSupplier(material.getOwnMaterialNumber());
-            request.setMaterialNumberCustomer(mprService.find(material, customerPartner).getPartnerMaterialNumber());
-            itemStockRequestMessage.getItemStock().add(request);
-        }
-        sendRequest(customerPartner, itemStockRequestMessage);
-
-    }
-
-    private ItemStockRequestMessage getItemStockRequestMessage(Partner partner) {
-        Partner mySelf = partnerService.getOwnPartnerEntity();
-        ItemStockRequestMessage.Key key = new ItemStockRequestMessage.Key(UUID.randomUUID(), mySelf.getBpnl(), partner.getBpnl());
-        while (itemStockRequestMessageService.find(key) != null) {
-            key.setMessageId(UUID.randomUUID());
-        }
-        ItemStockRequestMessage itemStockRequestMessage = new ItemStockRequestMessage();
-        itemStockRequestMessage.setKey(key);
-        return itemStockRequestMessage;
-    }
-
-    /**
-     * Convenience method to bundle the common parts in the workflow of
-     * the doRequestForMaterialItemStocks and doRequestForProductItemStocks methods.
-     *
-     * @param partner                 the partner
-     * @param itemStockRequestMessage the itemStockRequestMessage
-     */
-    private void sendRequest(Partner partner, ItemStockRequestMessage itemStockRequestMessage) {
-        itemStockRequestMessage = itemStockRequestMessageService.create(itemStockRequestMessage);
-        log.info("Created Message : \n" + (itemStockRequestMessage != null));
-        itemStockRequestMessage.setState(DT_RequestStateEnum.Working);
-        String[] data = edcAdapterService.getContractForItemStockApi(partner, DT_ApiMethodEnum.REQUEST);
-        if (data == null) {
-            log.error("Failed to obtain request api from " + partner.getEdcUrl());
-            itemStockRequestMessage.setState(DT_RequestStateEnum.Error);
-            itemStockRequestMessageService.update(itemStockRequestMessage);
-            return;
-        }
-        String authKey = data[0];
-        String authCode = data[1];
-        String endpoint = data[2];
-
-        try {
-            itemStockRequestMessage.setSentDateTime(new Date());
-            var requestDto = ItemStockRequestMessageDto.convertToDto(itemStockRequestMessage);
-            var response = edcAdapterService.postProxyPullRequest(endpoint, authKey, authCode,
-                objectMapper.writeValueAsString(requestDto));
-            if (response.isSuccessful()) {
-                itemStockRequestMessage.setState(DT_RequestStateEnum.Requested);
-            } else {
-                itemStockRequestMessage.setState(DT_RequestStateEnum.Error);
-            }
-            itemStockRequestMessageService.update(itemStockRequestMessage);
-            response.body().close();
-            log.info("Sent request \n" + requestDto);
-        } catch (Exception e) {
-            itemStockRequestMessage.setState(DT_RequestStateEnum.Error);
-            itemStockRequestMessageService.update(itemStockRequestMessage);
-            log.error("Failed to send request \n" + itemStockRequestMessage, e);
         }
     }
 
