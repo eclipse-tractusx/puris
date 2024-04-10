f3a66e38b5190b9eea01d323d9ea6962d4e99c77
ReneSchroederLJ <rene.schroeder@8020.eco>
2024-04-10 13:26:43 +0200

    feat: implementation of CRUD endpoints for material demand

---
+++ backend/src/main/java/org/eclipse/tractusx/puris/backend/demand/logic/services/ReportedDemandService.java
@@ -0,0 +1,50 @@
+/*
+Copyright (c) 2024 Volkswagen AG
+Copyright (c) 2024 Contributors to the Eclipse Foundation
+
+See the NOTICE file(s) distributed with this work for additional
+information regarding copyright ownership.
+
+This program and the accompanying materials are made available under the
+terms of the Apache License, Version 2.0 which is available at
+https://www.apache.org/licenses/LICENSE-2.0.
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+License for the specific language governing permissions and limitations
+under the License.
+
+SPDX-License-Identifier: Apache-2.0
+*/
+package org.eclipse.tractusx.puris.backend.demand.logic.services;
+
+import org.eclipse.tractusx.puris.backend.demand.domain.model.ReportedDemand;
+import org.eclipse.tractusx.puris.backend.demand.domain.repository.ReportedDemandRepository;
+import org.eclipse.tractusx.puris.backend.masterdata.domain.model.Partner;
+import org.eclipse.tractusx.puris.backend.masterdata.logic.service.PartnerService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReportedDemandService extends DemandService<ReportedDemand, ReportedDemandRepository> {
+
+    public ReportedDemandService(ReportedDemandRepository repository, PartnerService partnerService) {
+        super(repository, partnerService);
+    }
+
+    public boolean validate(ReportedDemand demand) {
+        Partner ownPartnerEntity = partnerService.getOwnPartnerEntity();
+        return 
+            demand.getMaterial() != null &&
+            demand.getPartner() != null &&
+            demand.getQuantity() > 0 && 
+            demand.getMeasurementUnit() != null && 
+            demand.getDay() != null && 
+            demand.getDemandCategoryCode() != null &&
+            demand.getDemandLocationBpns() != null &&
+            demand.getSupplierLocationBpns() != null &&
+            !demand.getPartner().equals(ownPartnerEntity) &&
+            ownPartnerEntity.getSites().stream().anyMatch(site -> site.getBpns().equals(demand.getSupplierLocationBpns())) &&
+            !ownPartnerEntity.getSites().stream().anyMatch(site -> site.getBpns().equals(demand.getDemandLocationBpns()));
+    }
+}
