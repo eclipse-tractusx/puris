e1737cf2c1b55c012d59e76b48b1de348a57f0bf
ReneSchroederLJ <rene.schroeder@8020.eco>
2024-05-23 09:13:40 +0200

    feat: updated user guide and added it to the app

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  assetsInclude: ['**/*.md'],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, './src'),
