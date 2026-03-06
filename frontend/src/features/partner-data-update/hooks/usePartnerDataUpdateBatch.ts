68979043eab7f4e769a9fe10aa09065bf02aea13
Tom <tom.meyer@isst.fraunhofer.de>
2026-03-06 18:39:29 +0100

    docs(batch): document changes

--- frontend/src/features/partner-data-update/hooks/usePartnerDataUpdateBatch.ts
+++ frontend/src/features/partner-data-update/hooks/usePartnerDataUpdateBatch.ts
@@ -26,7 +26,7 @@ import { BatchRunDto, BatchRunEntryDto } from '@models/types/data/batch';
 export const usePartnerDataUpdateBatch = (runId?: string, page?: number, size?: number) => {
   const base = config.app.BACKEND_BASE_URL;
   const endpoint = config.app.ENDPOINT_PARNTER_DATA_UPDATE_BATCH;
-  const runsUrl = `${base}${endpoint}?page=${page ?? 0}&size=${size ?? 20}`;
+  const runsUrl = `${base}${endpoint}?page=${page ?? 0}&size=${size ?? 20}&sort=startTime,desc`;
   const entriesUrl = runId ? `${base}${endpoint}/${runId}/entries?page=${page ?? 0}&size=${size ?? 20}` : undefined;
 
   const { data: runs, error: runsError, isLoading: isLoadingRuns, refresh: refreshRuns } = useFetch<{ content: BatchRunDto[], totalElements:number }>(runsUrl);
