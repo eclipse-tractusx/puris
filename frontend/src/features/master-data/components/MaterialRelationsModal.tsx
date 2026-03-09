91e1593714b9242f3e03caacbc3ce72feedef329
Olga Ivkovic <olga.ivkovic@8020.eco>
2026-03-09 11:00:28 +0100

    chore: update copyright for new files

--- frontend/src/features/master-data/components/MaterialRelationsModal.tsx
+++ frontend/src/features/master-data/components/MaterialRelationsModal.tsx
@@ -1,6 +1,5 @@
 /*
-Copyright (c) 2025 Volkswagen AG
-Copyright (c) 2025 Contributors to the Eclipse Foundation
+Copyright (c) 2026 Volkswagen AG
 
 See the NOTICE file(s) distributed with this work for additional
 information regarding copyright ownership.
