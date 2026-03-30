ccb60294f0db68aefa6213842f2bdcc0c938dc50
Aljaz Gobec <aljaz.gobec@8020.eco>
2026-03-30 14:20:47 +0200

    fix: remove unused line

--- backend/src/test/java/org/eclipse/tractusx/puris/backend/masterdata/logic/adapter/SingleLevelBomAsPlannedSammMapperTest.java
+++ backend/src/test/java/org/eclipse/tractusx/puris/backend/masterdata/logic/adapter/SingleLevelBomAsPlannedSammMapperTest.java
@@ -24,7 +24,6 @@ import static org.mockito.Mockito.*;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Date;
-import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
